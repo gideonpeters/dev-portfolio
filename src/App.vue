129e7d43b1b64f6935c68555c0799904be65950b
Gideon Peters <gideonpeters85@gmail.com>
2021-07-02 10:39:28 +0100

    feat(core): add new dist content for gh-pages

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
 	<div id="app1">
-		<div
-			class="logo-over"
-			:style="{ backgroundImage: `url(${bgImg})` }"
-		></div>
+		<div class="logo-over" :style="{ backgroundImage: `url(${bgImg})` }"></div>
 		<div class="overlay" v-if="overlay">
 			<div
 				class="d-flex flex-column text-center p-5"
@@ -52,28 +49,17 @@
 					<div class="social d-flex flex-column mb-3 text-center">
 						<div>
 							<a href="https://github.com/gideonpeters">
-								<img
-									color="primary"
-									src="@/assets/svg/github.svg"
-								/>
+								<img color="primary" src="@/assets/svg/github.svg" />
 							</a>
 						</div>
 						<div>
-							<a
-								href="https://www.linkedin.com/in/gideon-peters-8b813089/"
-							>
-								<img
-									color="primary"
-									src="@/assets/svg/linkedin.svg"
-								/>
+							<a href="https://www.linkedin.com/in/gideon-peters-8b813089/">
+								<img color="primary" src="@/assets/svg/linkedin.svg" />
 							</a>
 						</div>
 						<div>
 							<a href="https://medium.com/@gideonpeters85">
-								<img
-									color="primary"
-									src="@/assets/svg/medium.svg"
-								/>
+								<img color="primary" src="@/assets/svg/medium.svg" />
 							</a>
 						</div>
 					</div>
@@ -89,140 +75,140 @@
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			overlay: false,
-			routes: [
-				{ id: 1, name: "Home", route: "home" },
-				{ id: 2, name: "About Me", route: "about" },
-				{ id: 3, name: "My Skills", route: "skills" },
-				{ id: 4, name: "My Projects", route: "projects" },
-				{ id: 5, name: "Contact Me", route: "contact" }
-			]
-		};
-	},
-	computed: {
-		bgImg() {
-			let url;
-			if (this.$route.name == "skills") {
-				url = require("./assets/second-logo.png");
-			} else {
-				url = require("./assets/logo.png");
-			}
-
-			return url;
-		}
-	},
-	methods: {
-		toggleOverlay() {
-			this.overlay = !this.overlay;
+	export default {
+		data() {
+			return {
+				overlay: false,
+				routes: [
+					{ id: 1, name: "Home", route: "home" },
+					{ id: 2, name: "About Me", route: "about" },
+					{ id: 3, name: "My Skills", route: "skills" },
+					{ id: 4, name: "My Projects", route: "projects" },
+					{ id: 5, name: "Contact Me", route: "contact" },
+				],
+			};
 		},
-		goToPage(name) {
-			this.toggleOverlay();
-			this.$router.push({ name });
-		}
-	}
-};
+		computed: {
+			bgImg() {
+				let url;
+				if (this.$route.name == "skills") {
+					url = require("./assets/second-logo.png");
+				} else {
+					url = require("./assets/logo.png");
+				}
+
+				return url;
+			},
+		},
+		methods: {
+			toggleOverlay() {
+				this.overlay = !this.overlay;
+			},
+			goToPage(name) {
+				this.toggleOverlay();
+				this.$router.push({ name });
+			},
+		},
+	};
 </script>
 
 <style lang="scss">
-#app1 {
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: left;
-	color: white;
-	background-color: #27272b;
-	overflow-x: hidden;
-	height: 100vh;
-}
-
-.overlay {
-	height: 100vh;
-	width: 100vw;
-	position: absolute;
-	background: #64e6ab;
-	z-index: 9999999;
-	// opacity: 0.9;
-
-	.fs-5 {
-		font-size: 1.8rem;
+	#app1 {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: left;
+		color: white;
+		background-color: #27272b;
+		overflow-x: hidden;
+		height: 100vh;
 	}
-}
 
-.icon-menu {
-	display: flex;
-	justify-content: flex-end;
-	color: #64e6ab;
-}
-.nav-active {
-	color: white;
-}
+	.overlay {
+		height: 100vh;
+		width: 100vw;
+		position: absolute;
+		background: #64e6ab;
+		z-index: 9999999;
+		// opacity: 0.9;
 
-.logo-over {
-	position: absolute;
-	background-image: url("./assets/logo.png");
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-position: 85% center;
-	height: 400px;
-	width: 450px;
-	top: 200px;
-	right: 200px;
-}
-
-.b-view {
-	display: flex;
-	height: 100vh;
-	width: 100%;
-	overflow-x: hidden;
-	padding-top: 180px;
-	// overflow-y: scroll;
-	@media screen and(max-width: 700px) {
-		padding-top: 50px;
-	}
-}
-
-.sidebar {
-	min-width: 70px;
-	height: 100vh;
-	overflow: hidden;
-}
-
-#nav {
-	background-color: #202020;
-	// padding: 30px;
-	// position: absolute;
-	height: 100vh;
-	display: flex;
-	justify-content: space-between;
-	position: fixed;
-	// margin-bottom: auto;
-
-	a {
-		font-weight: bold;
-		color: #363636;
-
-		&.router-link-exact-active {
-			color: #42b983;
+		.fs-5 {
+			font-size: 1.8rem;
 		}
 	}
-}
 
-.social {
-	display: flex;
-	justify-content: space-between;
-	height: 20%;
-	// margin-top: auto;
-}
+	.icon-menu {
+		display: flex;
+		justify-content: flex-end;
+		color: #64e6ab;
+	}
+	.nav-active {
+		color: white;
+	}
 
-.logo-icon {
-	background-color: #1d1c1c;
-	background-image: url("./assets/logo.png");
-	background-size: 80%;
-	background-repeat: no-repeat;
-	background-position: center center;
-	height: 70px;
-	width: 70px;
-}
+	.logo-over {
+		position: absolute;
+		background-image: url("./assets/logo.png");
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: 85% center;
+		height: 400px;
+		width: 450px;
+		top: 200px;
+		right: 200px;
+	}
+
+	.b-view {
+		display: flex;
+		height: 100vh;
+		width: 100%;
+		overflow-x: hidden;
+		padding-top: 180px;
+		// overflow-y: scroll;
+		@media screen and(max-width: 700px) {
+			padding-top: 50px;
+		}
+	}
+
+	.sidebar {
+		min-width: 70px;
+		height: 100vh;
+		overflow: hidden;
+	}
+
+	#nav {
+		background-color: #202020;
+		// padding: 30px;
+		// position: absolute;
+		height: 100vh;
+		display: flex;
+		justify-content: space-between;
+		position: fixed;
+		// margin-bottom: auto;
+
+		a {
+			font-weight: bold;
+			color: #363636;
+
+			&.router-link-exact-active {
+				color: #42b983;
+			}
+		}
+	}
+
+	.social {
+		display: flex;
+		justify-content: space-between;
+		height: 20%;
+		// margin-top: auto;
+	}
+
+	.logo-icon {
+		background-color: #1d1c1c;
+		background-image: url("./assets/logo.png");
+		background-size: 80%;
+		background-repeat: no-repeat;
+		background-position: center center;
+		height: 70px;
+		width: 70px;
+	}
 </style>
